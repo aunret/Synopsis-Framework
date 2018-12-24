be0e81bc11d2d0e7283e8c3df3c02d3bad746eb0
vade <vade@vade.info>
2018-12-24 01:31:00 -0500

    Output probablities and labels as description

--- Synopsis/Synopsis/MobileNetV2 CoreML/GPUVisionMobileNetV2.m
+++ Synopsis/Synopsis/MobileNetV2 CoreML/GPUVisionMobileNetV2.m
@@ -30,20 +30,35 @@
 
 @property (readwrite, strong) smoosh_5tasks_softmax_no_labels_subtasks* smooshNetCoreMLModel;
 
-@property (readwrite, strong) NSMutableArray<NSNumber*>* averageFeatureVec;
-@property (readwrite, strong) NSMutableArray<SynopsisDenseFeature*>* windowAverages;
-@property (readwrite, strong) NSMutableArray<NSValue*>* windowAverageTimes;
+//@property (readwrite, strong) NSMutableArray<NSNumber*>* averageFeatureVec;
+@property (readwrite, strong) SynopsisDenseFeature* averageFeatureVec;
+@property (readwrite, strong) NSMutableArray<SynopsisDenseFeature*>* featureVectorWindowAverages;
+@property (readwrite, strong) NSMutableArray<NSValue*>* featureVectorWindowAveragesTimes;
+@property (readwrite, strong) NSMutableArray<SynopsisSlidingWindow*>* featureVectorWindows;
+
+@property (readwrite, strong) SynopsisDenseFeature* averageProbabilities;
+@property (readwrite, strong) NSMutableArray<SynopsisDenseFeature*>* probabilityWindowAverages;
+@property (readwrite, strong) NSMutableArray<NSValue*>* probabilityWindowAveragesTimes;
+@property (readwrite, strong) NSMutableArray<SynopsisSlidingWindow*>* probabilityWindows;
+
+@property (readwrite, strong) NSArray* labels;
 @property (readwrite, strong) NSArray* sceneAttributeLabels;
 @property (readwrite, strong) NSArray* styleAttributeLabels;
 @property (readwrite, strong) NSArray* placesLabels;
 @property (readwrite, strong) NSArray* objectsLabels;
 @property (readwrite, strong) NSArray* objectAttributeLabels;
 
-@property (readwrite, strong) NSMutableArray<SynopsisSlidingWindow*>* windows;
-
 @end
 
 @implementation GPUVisionMobileNetV2
+
+
+static NSUInteger sceneCount = 102;
+static NSUInteger styleCount = 20;
+static NSUInteger placesCount = 365;
+static NSUInteger objectCount = 80;
+static NSUInteger objectAttributeCount = 204;
+static NSUInteger featureVectorCount = 1280;
 
     // GPU backed modules init with an options dict for Metal Device bullshit
 - (instancetype) initWithQualityHint:(SynopsisAnalysisQualityHint)qualityHint device:(id<MTLDevice>)device
@@ -70,14 +85,13 @@
         
         NSString* allLabelsFlat = [NSString stringWithContentsOfURL:labelsURL encoding:NSUTF8StringEncoding error:&error];
         
-        NSArray* allLabels = [allLabelsFlat componentsSeparatedByCharactersInSet:[NSCharacterSet newlineCharacterSet]];
-                
-        self.sceneAttributeLabels = [allLabels subarrayWithRange:NSMakeRange(0, 102)];
-        self.styleAttributeLabels = [allLabels subarrayWithRange:NSMakeRange(102, 20)];
-        self.placesLabels = [allLabels subarrayWithRange:NSMakeRange(102 + 20, 365)];
-        self.objectsLabels = [allLabels subarrayWithRange:NSMakeRange(102 + 20 + 365, 80)];
-        self.objectAttributeLabels = [allLabels subarrayWithRange:NSMakeRange(102 + 20 + 365 + 80, 204)];
-        
+        self.labels = [allLabelsFlat componentsSeparatedByCharactersInSet:[NSCharacterSet newlineCharacterSet]];
+        self.sceneAttributeLabels = [self.labels subarrayWithRange:NSMakeRange(0, sceneCount)];
+        self.styleAttributeLabels = [self.labels subarrayWithRange:NSMakeRange(sceneCount, styleCount)];
+        self.placesLabels = [self.labels subarrayWithRange:NSMakeRange(sceneCount + styleCount, placesCount)];
+        self.objectsLabels = [self.labels subarrayWithRange:NSMakeRange(sceneCount + styleCount + placesCount, objectCount)];
+        self.objectAttributeLabels = [self.labels subarrayWithRange:NSMakeRange(sceneCount + styleCount + placesCount + objectCount, objectAttributeCount)];
+//
         if(error)
         {
             NSLog(@"Error: %@", error);
@@ -111,15 +125,21 @@
 - (void) beginAndClearCachedResults
 {
     self.averageFeatureVec = nil;
-    
-    self.windowAverages = [NSMutableArray new];
-    self.windowAverageTimes = [NSMutableArray new];
-    self.windows = [NSMutableArray new];
-    
+    self.featureVectorWindowAverages = [NSMutableArray new];
+    self.featureVectorWindowAveragesTimes = [NSMutableArray new];
+    self.featureVectorWindows = [NSMutableArray new];
+
+    self.averageProbabilities = nil;
+    self.probabilityWindowAverages = [NSMutableArray new];
+    self.probabilityWindowAveragesTimes = [NSMutableArray new];
+    self.probabilityWindows = [NSMutableArray new];
+
     for(NSUInteger i = 0; i < numWindows; i++)
     {
         SynopsisSlidingWindow* aWindow = [[SynopsisSlidingWindow alloc] initWithLength:10 offset:stride * i];
-        [self.windows addObject:aWindow];
+        SynopsisSlidingWindow* bWindow = [[SynopsisSlidingWindow alloc] initWithLength:10 offset:stride * i];
+        [self.featureVectorWindows addObject:aWindow];
+        [self.probabilityWindows addObject:bWindow];
     }
 }
 
@@ -157,48 +177,128 @@
             VNCoreMLFeatureValueObservation* featureOutput = [request results][3];
 
             MLMultiArray* featureVector = featureOutput.featureValue.multiArrayValue;
+            MLMultiArray* scene_attrs_prob = scene_attrs.featureValue.multiArrayValue;
+            MLMultiArray* style_attrs_prob = style_atrs.featureValue.multiArrayValue;
+            MLMultiArray* places_prob = places.featureValue.multiArrayValue;
+            MLMultiArray* objects_prob = objects.featureValue.multiArrayValue;
+            MLMultiArray* objects_attrs_prob = objects_attrs.featureValue.multiArrayValue;
             
-            NSMutableArray<NSNumber*>*vec = [NSMutableArray new];
+            NSMutableArray<NSNumber*>*featureVec_NSNumber = [NSMutableArray new];
+            NSMutableArray<NSNumber*>*scene_attrs_prob_NSNumber = [NSMutableArray new];
+            NSMutableArray<NSNumber*>*style_atrs_prob_NSNumber = [NSMutableArray new];
+            NSMutableArray<NSNumber*>*places_prob_NSNumber = [NSMutableArray new];
+            NSMutableArray<NSNumber*>*objects_prob_NSNumber = [NSMutableArray new];
+            NSMutableArray<NSNumber*>*objects_attrs_prob_NSNumber = [NSMutableArray new];
+
+#pragma mark - This is beyond wasteful and shitty
+           
+            for(NSUInteger i = 0; i < featureVectorCount; i++)
+            {
+                featureVec_NSNumber[i] = [featureVector objectForKeyedSubscript:@[@(0), @(0), @(i), @(0), @(0)] ];
+            }
+            
+            SynopsisDenseFeature* denseFeatureVector = [[SynopsisDenseFeature alloc] initWithFeatureArray:featureVec_NSNumber];
+            
+            for(NSUInteger i = 0; i < sceneCount; i++)
+            {
+                scene_attrs_prob_NSNumber[i] = [scene_attrs_prob objectForKeyedSubscript:@[@(0), @(0), @(i), @(0), @(0)] ];
+            }
+           
+            for(NSUInteger i = 0; i < styleCount; i++)
+            {
+                style_atrs_prob_NSNumber[i] = [style_attrs_prob objectForKeyedSubscript:@[@(0), @(0), @(i), @(0), @(0)] ];
+            }
+           
+            for(NSUInteger i = 0; i < placesCount; i++)
+            {
+                places_prob_NSNumber[i] = [places_prob objectForKeyedSubscript:@[@(0), @(0), @(i), @(0), @(0)] ];
+            }
+           
+            for(NSUInteger i = 0; i < objectCount; i++)
+            {
+                objects_prob_NSNumber[i] = [objects_prob objectForKeyedSubscript:@[@(0), @(0), @(i), @(0), @(0)] ];
+            }
+
+            for(NSUInteger i = 0; i < objectAttributeCount; i++)
+            {
+                objects_attrs_prob_NSNumber[i] = [objects_attrs_prob objectForKeyedSubscript:@[@(0), @(0), @(i), @(0), @(0)] ];
+            }
+
+            NSArray* allProbabilities_NSNumber = [[[[scene_attrs_prob_NSNumber arrayByAddingObjectsFromArray:style_atrs_prob_NSNumber]
+                                           arrayByAddingObjectsFromArray:places_prob_NSNumber]
+                                          arrayByAddingObjectsFromArray:objects_prob_NSNumber]
+                                         arrayByAddingObjectsFromArray:objects_attrs_prob_NSNumber];
+            
+            SynopsisDenseFeature* denseAllProbabilities = [[SynopsisDenseFeature alloc] initWithFeatureArray:allProbabilities_NSNumber];
+
             
             if(self.averageFeatureVec == nil)
             {
-                for(NSUInteger i = 0; i < 1280; i++)
-                {
-                   
-                    vec[i] = [featureVector objectForKeyedSubscript:@[@(0), @(0), @(i), @(0), @(0)] ];
-                }
-                
-                self.averageFeatureVec = vec;
+                self.averageFeatureVec = [[SynopsisDenseFeature alloc] initWithFeatureArray:featureVec_NSNumber];
             }
-            
             else
             {
-                for(NSUInteger i = 0; i < self.averageFeatureVec.count; i++)
-                {
-                    NSNumber* avgFeatureValue = self.averageFeatureVec[i];
-                    NSNumber* featureValue = [featureVector objectForKeyedSubscript:@[@(0), @(0), @(i), @(0), @(0)] ];
-                    
-                    self.averageFeatureVec[i] = @( (avgFeatureValue.floatValue + featureValue.floatValue) * 0.5 );
-                    vec[i] = featureValue;
-                }
+                self.averageFeatureVec = [SynopsisDenseFeature denseFeatureByAveragingFeature:self.averageFeatureVec withFeature:denseFeatureVector];
+            }
+            
+            if(self.averageProbabilities == nil)
+            {
+                self.averageProbabilities = [[SynopsisDenseFeature alloc] initWithFeatureArray:allProbabilities_NSNumber];
+            }
+            else
+            {
+                self.averageProbabilities = [SynopsisDenseFeature denseFeatureByAveragingFeature:self.averageProbabilities withFeature:denseAllProbabilities];
             }
                         
-            SynopsisDenseFeature* denseFeatureVector = [[SynopsisDenseFeature alloc] initWithFeatureArray:vec];
             
-            metadata = [NSMutableDictionary dictionary];
-            
-            [self.windows enumerateObjectsUsingBlock:^(SynopsisSlidingWindow * _Nonnull window, NSUInteger idx, BOOL * _Nonnull stop) {
+            [self.featureVectorWindows enumerateObjectsUsingBlock:^(SynopsisSlidingWindow * _Nonnull window, NSUInteger idx, BOOL * _Nonnull stop) {
                 SynopsisDenseFeature* possible = [window appendFeature:denseFeatureVector];
                 if(possible != nil)
                 {
-                    [self.windowAverages addObject:possible];
-                    [self.windowAverageTimes addObject:[NSValue valueWithCMTime:frame.presentationTimeStamp]];
+                    [self.featureVectorWindowAverages addObject:possible];
+                    [self.featureVectorWindowAveragesTimes addObject:[NSValue valueWithCMTime:frame.presentationTimeStamp]];
+                }
+            }];
+          
+            [self.probabilityWindows enumerateObjectsUsingBlock:^(SynopsisSlidingWindow * _Nonnull window, NSUInteger idx, BOOL * _Nonnull stop) {
+                SynopsisDenseFeature* possible = [window appendFeature:denseFeatureVector];
+                if(possible != nil)
+                {
+                    [self.probabilityWindowAverages addObject:possible];
+                    [self.probabilityWindowAveragesTimes addObject:[NSValue valueWithCMTime:frame.presentationTimeStamp]];
                 }
             }];
             
-            metadata[kSynopsisStandardMetadataFeatureVectorDictKey] = vec;
-//            metadata[kSynopsisStandardMetadataDescriptionDictKey] = labels;
+            NSArray* sceneLabels = [self topLabelForScores:[NSDictionary dictionaryWithObjects:scene_attrs_prob_NSNumber forKeys:self.sceneAttributeLabels]
+                                            withThreshhold:0.0
+                                             maxLabelCount:3];
             
+            NSArray* styleLabels = [self topLabelForScores:[NSDictionary dictionaryWithObjects:style_atrs_prob_NSNumber forKeys:self.styleAttributeLabels]
+                                            withThreshhold:0.0
+                                             maxLabelCount:3];
+
+            NSArray* placesLabels = [self topLabelForScores:[NSDictionary dictionaryWithObjects:places_prob_NSNumber forKeys:self.placesLabels]
+                                            withThreshhold:0.0
+                                             maxLabelCount:3];
+
+            NSArray* objLabels = [self topLabelForScores:[NSDictionary dictionaryWithObjects:objects_prob_NSNumber forKeys:self.objectsLabels]
+                                             withThreshhold:0.0
+                                              maxLabelCount:3];
+            
+            NSArray* objAttrLabels = [self topLabelForScores:[NSDictionary dictionaryWithObjects:objects_attrs_prob_NSNumber forKeys:self.objectAttributeLabels]
+                                          withThreshhold:0.0
+                                           maxLabelCount:3];
+
+            NSArray* labels = [[[[sceneLabels arrayByAddingObjectsFromArray:styleLabels]
+                                 arrayByAddingObjectsFromArray:placesLabels]
+                                arrayByAddingObjectsFromArray:objLabels]
+                               arrayByAddingObjectsFromArray:objAttrLabels];
+
+            metadata = [NSMutableDictionary dictionary];
+            metadata[kSynopsisStandardMetadataFeatureVectorDictKey] = featureVec_NSNumber;
+            metadata[kSynopsisStandardMetadataProbabilitiesDictKey] = allProbabilities_NSNumber;
+            metadata[kSynopsisStandardMetadataDescriptionDictKey] = labels;
+
             if(completionBlock)
             {
                 completionBlock(metadata, nil);
@@ -226,21 +326,88 @@
 
 - (NSDictionary*) finalizedAnalysisMetadata;
 {
-    NSMutableArray* windowAverages = [NSMutableArray arrayWithCapacity:self.windowAverages.count];
+//    NSMutableArray* windowAverages = [NSMutableArray arrayWithCapacity:self.featureVectorWindowAverages.count];
+//
+//    [self.featureVectorWindowAverages enumerateObjectsUsingBlock:^(SynopsisDenseFeature * _Nonnull feature, NSUInteger idx, BOOL * _Nonnull stop) {
+//        NSValue* windowTime = [self.featureVectorWindowAveragesTimes objectAtIndex:idx];
+//
+//        [windowAverages addObject: @{ @"Feature" : [feature arrayValue],
+//                                      @"Time" : (NSDictionary*) CFBridgingRelease(CMTimeCopyAsDictionary([windowTime CMTimeValue], kCFAllocatorDefault)),
+//                                      }];
+//    }];
+   
+    NSArray<NSNumber*>* averageProbabilities = self.averageProbabilities.arrayValue;
     
-    [self.windowAverages enumerateObjectsUsingBlock:^(SynopsisDenseFeature * _Nonnull feature, NSUInteger idx, BOOL * _Nonnull stop) {
-        NSValue* windowTime = [self.windowAverageTimes objectAtIndex:idx];
+    NSArray* prob_sceneAttributeLabels = [averageProbabilities subarrayWithRange:NSMakeRange(0, sceneCount)];
+    NSArray* prob_styleAttributeLabels = [averageProbabilities subarrayWithRange:NSMakeRange(sceneCount, styleCount)];
+    NSArray* prob_placesLabels = [averageProbabilities subarrayWithRange:NSMakeRange(sceneCount + styleCount, placesCount)];
+    NSArray* prob_objectsLabels = [averageProbabilities subarrayWithRange:NSMakeRange(sceneCount + styleCount + placesCount, objectCount)];
+    NSArray* prob_objectAttributeLabels = [averageProbabilities subarrayWithRange:NSMakeRange(sceneCount + styleCount + placesCount + objectCount, objectAttributeCount)];
+
+    
+    NSArray* sceneLabels = [self topLabelForScores:[NSDictionary dictionaryWithObjects:prob_sceneAttributeLabels forKeys:self.sceneAttributeLabels]
+                                    withThreshhold:0.0
+                                     maxLabelCount:3];
+    
+    NSArray* styleLabels = [self topLabelForScores:[NSDictionary dictionaryWithObjects:prob_styleAttributeLabels forKeys:self.styleAttributeLabels]
+                                    withThreshhold:0.0
+                                     maxLabelCount:3];
+    
+    NSArray* placesLabels = [self topLabelForScores:[NSDictionary dictionaryWithObjects:prob_placesLabels forKeys:self.placesLabels]
+                                     withThreshhold:0.0
+                                      maxLabelCount:3];
+    
+    NSArray* objLabels = [self topLabelForScores:[NSDictionary dictionaryWithObjects:prob_objectsLabels forKeys:self.objectsLabels]
+                                  withThreshhold:0.0
+                                   maxLabelCount:3];
+    
+    NSArray* objAttrLabels = [self topLabelForScores:[NSDictionary dictionaryWithObjects:prob_objectAttributeLabels forKeys:self.objectAttributeLabels]
+                                      withThreshhold:0.0
+                                       maxLabelCount:3];
+    
+    NSArray* labels = [[[[sceneLabels arrayByAddingObjectsFromArray:styleLabels]
+                         arrayByAddingObjectsFromArray:placesLabels]
+                        arrayByAddingObjectsFromArray:objLabels]
+                       arrayByAddingObjectsFromArray:objAttrLabels];
+
+    return @{
+             kSynopsisStandardMetadataFeatureVectorDictKey : (self.averageFeatureVec) ? self.averageFeatureVec.arrayValue : @[ ],
+             kSynopsisStandardMetadataProbabilitiesDictKey : (averageProbabilities) ? averageProbabilities : @[ ],
+//             kSynopsisStandardMetadataInterestingFeaturesAndTimesDictKey  : (windowAverages) ? windowAverages : @[ ],
+             kSynopsisStandardMetadataDescriptionDictKey: (labels) ? labels : @[ ],
+             };
+}
+
+- (NSArray<NSString*>*) topLabelForScores:(NSDictionary*)scoresDict withThreshhold:(float)thresh maxLabelCount:(NSUInteger)maxLabels
+{
+    NSMutableDictionary* scores = [scoresDict mutableCopy];
+    
+    NSArray* sortedScores = [[scores allValues] sortedArrayUsingComparator:^NSComparisonResult(id  _Nonnull obj1, id  _Nonnull obj2) {
         
-        [windowAverages addObject: @{ @"Feature" : [feature arrayValue],
-                                      @"Time" : (NSDictionary*) CFBridgingRelease(CMTimeCopyAsDictionary([windowTime CMTimeValue], kCFAllocatorDefault)),
-                                      }];
+        if([obj1 floatValue] > [obj2 floatValue])
+            return NSOrderedAscending;
+        else if([obj1 floatValue] < [obj2 floatValue])
+            return NSOrderedDescending;
+        
+        return NSOrderedSame;
+    }] ;
+    
+        //    NSString* topFrameLabel = nil;
+    NSMutableArray* top3 = [NSMutableArray array];
+        // Modulate percentage based off of number of possible categories?
+    [sortedScores enumerateObjectsUsingBlock:^(NSNumber*  _Nonnull score, NSUInteger idx, BOOL * _Nonnull stop) {
+        if(idx >= (maxLabels - 1) )
+            *stop = YES;
+        
+        if(score.floatValue >= (thresh / scores.allKeys.count))
+        {
+            NSString* scoreLabel = [[scores allKeysForObject:score] firstObject];
+            [top3 addObject:scoreLabel];
+        }
     }];
     
-    return @{
-             kSynopsisStandardMetadataFeatureVectorDictKey : (self.averageFeatureVec) ? self.averageFeatureVec : @[ ],
-             kSynopsisStandardMetadataInterestingFeaturesAndTimesDictKey  : (windowAverages) ? windowAverages : @[ ],
-//             kSynopsisStandardMetadataDescriptionDictKey: (self.labels) ? self.labels : @[ ],
-             };
+    
+    return top3;
 }
 
 @end
