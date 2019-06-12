f687f9202bcd6d2bb3d9de5f3a4f2d7cfd8ea496
vade <vade@vade.info>
2019-06-12 16:38:20 -0400

    Begin integration of the new interim model from autoML

---
+++ Synopsis/Synopsis/CinemaNet AutoML/CinemaNetModuleV1.h
@@ -0,0 +1,17 @@
+//
+//  CinemaNetModuleV1.h
+//  Synopsis-Framework
+//
+//  Created by vade on 6/12/19.
+//  Copyright © 2019 v002. All rights reserved.
+//
+
+#import "GPUModule.h"
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface CinemaNetModuleV1 : GPUModule
+
+@end
+
+NS_ASSUME_NONNULL_END
