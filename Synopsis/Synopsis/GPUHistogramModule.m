08399cf10b801c26601d15b7c08c0adb3015af04
vade <vade@vade.info>
2019-10-11 14:01:03 -0400

    Fix bug in GPU histogram module and fix averaging method. Now fully functional and mostly matches OpenCV CPU histogram module. Nice.

--- Synopsis/Synopsis/GPUHistogramModule.m
+++ Synopsis/Synopsis/GPUHistogramModule.m
@@ -12,7 +12,14 @@
 @interface GPUHistogramModule ()
 @property (readwrite, strong) MPSImageHistogram* histogramOp;
 @property (readwrite, assign) MPSImageHistogramInfo* histogramInfo;
+
+@property (readwrite, strong) SynopsisDenseFeature* averageRHistogram;
+@property (readwrite, strong) SynopsisDenseFeature* averageGHistogram;
+@property (readwrite, strong) SynopsisDenseFeature* averageBHistogram;
+
 @end
+
+#define kGPUHistogramModuleHistogramSize 256
 
 @implementation GPUHistogramModule
 
@@ -26,7 +33,7 @@
         
         vector_float4 max = {1, 1, 1, 1};
         vector_float4 min = {0, 0, 0, 0};
-        self.histogramInfo->numberOfHistogramEntries = 256;
+        self.histogramInfo->numberOfHistogramEntries = kGPUHistogramModuleHistogramSize;
         self.histogramInfo->maxPixelValue = max;
         self.histogramInfo->minPixelValue = min;
         self.histogramInfo->histogramForAlpha = NO;
@@ -59,7 +66,9 @@
 
 - (void) beginAndClearCachedResults
 {
-    
+    self.averageRHistogram = nil;
+    self.averageGHistogram = nil;
+    self.averageBHistogram = nil;
 }
 
 - (void) analyzedMetadataForCurrentFrame:(id<SynopsisVideoFrame>)frame previousFrame:(id<SynopsisVideoFrame>)lastFrame commandBuffer:(id<MTLCommandBuffer>)buffer completionBlock:(GPUModuleCompletionBlock)completionBlock;
@@ -67,7 +76,7 @@
     SynopsisVideoFrameMPImage* frameMPImage = (SynopsisVideoFrameMPImage*)frame;
     
     id<MTLBuffer> histogramResult = [buffer.device newBufferWithLength:[self.histogramOp histogramSizeForSourceFormat:MTLPixelFormatBGRA8Unorm] options:MTLResourceStorageModeShared];
-    
+        
     [self.histogramOp encodeToCommandBuffer:buffer
                               sourceTexture:frameMPImage.mpsImage.texture
                                   histogram:histogramResult
@@ -79,11 +88,16 @@
          // by reading old work and doing dumb math
          dispatch_async(self.completionQueue, ^{
              
+             NSMutableArray<NSNumber*>* frameRHistogram = [NSMutableArray arrayWithCapacity:kGPUHistogramModuleHistogramSize];
+             NSMutableArray<NSNumber*>* frameGHistogram = [NSMutableArray arrayWithCapacity:kGPUHistogramModuleHistogramSize];
+             NSMutableArray<NSNumber*>* frameBHistogram = [NSMutableArray arrayWithCapacity:kGPUHistogramModuleHistogramSize];
+
+             NSMutableArray<NSArray<NSNumber*>*>* histoGramTuples = [NSMutableArray arrayWithCapacity:kGPUHistogramModuleHistogramSize];
+
+             
              uint32_t* start = [histogramResult contents];
              NSUInteger buffLength = [histogramResult length];
-             
-             NSMutableArray<NSMutableArray<NSNumber*>*>* histogramValues = [NSMutableArray arrayWithCapacity:256];
-             
+                          
              size_t uint32tsize = sizeof(uint32_t);
              
              // 4 bytes for uint32tsize typically
@@ -92,18 +106,12 @@
              //3 Channels
              buffLength = buffLength / 3;
              
-//             float rSum = 0.0;
+             assert(buffLength == kGPUHistogramModuleHistogramSize);
+             
              float rMax = 0.0;
-//             float rMin = 1.0;
-
-//             float gSum = 0.0;
              float gMax = 0.0;
-//             float gMin = 1.0;
-             
-//             float bSum = 0.0;
              float bMax = 0.0;
-//             float bMin = 1.0;
-             
+
              for(int i = 0; i < buffLength; i++)
              {
                  // Planar histogram offsets?
@@ -114,59 +122,80 @@
                  float r = (float)rUI;
                  float g = (float)gUI;
                  float b = (float)bUI;
-                 
-//                 r = pow(r, 1.0/2.2);
-//                 g = pow(g, 1.0/2.2);
-//                 b = pow(b, 1.0/2.2);
 
-//                 rSum += r;
-//                 gSum += g;
-//                 bSum += b;
-                 
                  rMax = MAX(r, rMax);
                  gMax = MAX(g, gMax);
                  bMax = MAX(b, bMax);
+                                  
+                 [frameRHistogram addObject: @(r)];
+                 [frameGHistogram addObject: @(g)];
+                 [frameBHistogram addObject: @(b)];
+             }
 
-//                 rMin = MIN(r, rMin);
-//                 gMin = MIN(g, gMin);
-//                 bMin = MIN(b, bMin);
-                 
-                 NSArray* channelValuesForRow = @[ @(r), @(g), @(b)];
-                 
-                 [histogramValues addObject:[channelValuesForRow mutableCopy]];
+             for(int i = 0; i < buffLength; i++)
+             {
+                 frameRHistogram[i] = @( frameRHistogram[i].floatValue / rMax );
+                 frameGHistogram[i] = @( frameGHistogram[i].floatValue / gMax );
+                 frameBHistogram[i] = @( frameBHistogram[i].floatValue / bMax );
              }
              
-//             float maxSum = MAX(rSum, MAX(bSum, gSum));
-             float max = MAX(rMax, MAX(bMax, gMax));
-//             float min = MAX(rMin, MAX(bMin, gMin));
-
-             for(NSMutableArray<NSNumber*>* tuplet in histogramValues)
+             if(self.averageRHistogram == nil)
              {
-                 tuplet[0] = @( tuplet[0].floatValue / max );
-                 tuplet[1] = @( tuplet[1].floatValue / max );
-                 tuplet[2] = @( tuplet[2].floatValue / max );
-                 
-//                 tuplet[0] = @( [self remapValue:tuplet[0].floatValue oldLow:min oldHigh:max newLow:0 newHigh:1] );
-//                 tuplet[1] = @( [self remapValue:tuplet[1].floatValue oldLow:min oldHigh:max newLow:0 newHigh:1] );
-//                 tuplet[2] = @( [self remapValue:tuplet[2].floatValue oldLow:min oldHigh:max newLow:0 newHigh:1] );
+                 self.averageRHistogram = [[SynopsisDenseFeature alloc] initWithFeatureArray:frameRHistogram];
+             }
+             else
+             {
+                 self.averageRHistogram = [SynopsisDenseFeature denseFeatureByAveragingFeature:self.averageRHistogram withFeature:[[SynopsisDenseFeature alloc] initWithFeatureArray:frameRHistogram]];
              }
              
-             if(completionBlock)
+             if(self.averageGHistogram == nil)
              {
-                 completionBlock( @{[self moduleName] : histogramValues} , nil);
+                 self.averageGHistogram = [[SynopsisDenseFeature alloc] initWithFeatureArray:frameGHistogram];
              }
+             else
+             {
+                 self.averageGHistogram = [SynopsisDenseFeature denseFeatureByAveragingFeature:self.averageGHistogram withFeature:[[SynopsisDenseFeature alloc] initWithFeatureArray:frameGHistogram]];
+             }
+             
+            if(self.averageBHistogram == nil)
+            {
+              self.averageBHistogram = [[SynopsisDenseFeature alloc] initWithFeatureArray:frameBHistogram];
+            }
+            else
+            {
+              self.averageBHistogram = [SynopsisDenseFeature denseFeatureByAveragingFeature:self.averageBHistogram withFeature:[[SynopsisDenseFeature alloc] initWithFeatureArray:frameBHistogram]];
+            }
+             
+            for(int i = 0; i < buffLength; i++)
+            {
+                [histoGramTuples addObject: @[ frameRHistogram[i], frameGHistogram[i], frameBHistogram[i] ] ];
+            }
+            
+            if(completionBlock)
+            {
+                completionBlock( @{[self moduleName] : histoGramTuples} , nil);
+            }
          });
      }];
 }
 
-- (float) remapValue:(float)value oldLow:(float)low1 oldHigh:(float)high1 newLow:(float)low2 newHigh:(float)high2
-{
-    return (low2 + (value - low1) * (high2 - low2) / (high1 - low1) );
-}
 
 - (NSDictionary*) finalizedAnalysisMetadata;
 {
-    return nil;
+    
+    NSArray<NSNumber*>* rHistogram = self.averageRHistogram.arrayValue;
+    NSArray<NSNumber*>* gHistogram = self.averageGHistogram.arrayValue;
+    NSArray<NSNumber*>* bHistogram = self.averageBHistogram.arrayValue;
+
+    NSMutableArray<NSArray<NSNumber*>*>* histoGramTuples = [NSMutableArray arrayWithCapacity:kGPUHistogramModuleHistogramSize];
+
+    for(int i = 0; i < kGPUHistogramModuleHistogramSize; i++)
+    {
+        [histoGramTuples addObject: @[ rHistogram[i], gHistogram[i], bHistogram[i] ] ];
+    }
+    
+    
+    return @{[self moduleName] : histoGramTuples};
 }
 
 @end
