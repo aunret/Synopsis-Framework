84ad314bab11b2081f193f9f8a85a12c32f317e7
ray cutler <ray@vidvox.net>
2019-12-02 15:59:46 -0500

    work on framework
    added async init methods for SynopsisMetadataItem

--- Synopsis/Synopsis/SynopsisMetadataItem.h
+++ Synopsis/Synopsis/SynopsisMetadataItem.h
@@ -10,16 +10,32 @@
 #import <AVFoundation/AVFoundation.h>
 
 @class SynopsisMetadataDecoder;
+@class SynopsisMetadataItem;
+
+
+
+typedef void(^SynopsisMetadataItemCompletionHandler)(SynopsisMetadataItem *completedItem);
+
+
+
+
 // Thin wrapper for NSMetadataItem to implement Key Value access to HFS + Extended attribute's (which Synopsis Can leverage)
 
+
+
+
 @interface SynopsisMetadataItem : NSObject<NSCopying>
+
+- (instancetype) initWithURL:(NSURL *)url;
+//- (instancetype) initWithAsset:(AVAsset *)asset;
+
+- (instancetype) initWithURL:(NSURL *)url onQueue:(dispatch_queue_t)q completionHandler:(SynopsisMetadataItemCompletionHandler)ch;
+
 @property (readonly) NSURL* url;
 @property (readonly) AVAsset* asset;
+@property (readonly) BOOL loaded;
 
 // Re-use this during playback if you can!
 @property (readonly) SynopsisMetadataDecoder* decoder;
-
-- (instancetype) initWithURL:(NSURL *)url;
-- (instancetype) initWithAsset:(AVAsset *)asset;
 
 @end
