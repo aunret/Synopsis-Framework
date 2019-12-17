3dbd3a37e5494491173b5e4bba3e027c8b89c7cb
vade <vade@vade.info>
2019-12-17 16:56:57 -0500

    Update CinemaNet Module.

--- Synopsis/Synopsis/CinemaNet/CinemaNetModuleV1.m
+++ Synopsis/Synopsis/CinemaNet/CinemaNetModuleV1.m
@@ -345,6 +345,8 @@
             {
                 float featureSimilarity = compareFeaturesCosineSimilarity(self.lastFrameFeatureVec, denseFeature);
                 
+                featureSimilarity = 1.0 - featureSimilarity;
+                
                 SynopsisDenseFeature* denseSimilarity = [[SynopsisDenseFeature alloc] initWithFeatureArray:@[@(featureSimilarity)] forMetadataKey:kSynopsisStandardMetadataSimilarityFeatureVectorDictKey];
                 
                 if ( self.similarityFeatureVec )
@@ -361,6 +363,8 @@
             {
                 float featureSimilarity = compareFeaturesCosineSimilarity(self.lastFrameProbabilities, denseProbabilities);
                                
+                featureSimilarity = 1.0 - featureSimilarity;
+
                 SynopsisDenseFeature* denseSimilarity = [[SynopsisDenseFeature alloc] initWithFeatureArray:@[@(featureSimilarity)] forMetadataKey:kSynopsisStandardMetadataSimilarityProbabilitiesDictKey];
                 
                 if ( self.similarityProbabilities )
@@ -423,7 +427,10 @@
         [predictedLabels addObjectsFromArray:topLabels];
     }];
     
-
+    // This vector size is greatly affected by the duration of the source clip
+    // Too large, and you get resizing issues
+    // Too small, and you smooth our important features
+    // This is vv WIP:
     [self.similarityFeatureVec resizeTo:1024];
     [self.similarityProbabilities resizeTo:1024];
 
